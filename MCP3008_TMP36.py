651782cbbccab38098b83de7248f964e9d6600b6
Matt Hawkins <matthawkinsuk@gmail.com>
2013-10-13 23:04:24 +0100

    Added some comments

--- MCP3008_TMP36.py
+++ MCP3008_TMP36.py
@@ -5,11 +5,11 @@ import os
 spi = spidev.SpiDev()
 spi.open(0,0)
 
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
-def readadc(adcnum):
-  if ((adcnum > 7) or (adcnum < 0)):
+# Function to read SPI data from MCP3008 chip
+def ReadChannel(channel):
+  if ((channel > 7) or (channel < 0)):
     return -1
-  r = spi.xfer2([1,(8+adcnum)<<4,0])
+  r = spi.xfer2([1,(8+channel)<<4,0])
   adcout = ((r[1]&3) << 8) + r[2]
   return adcout
 
@@ -19,19 +19,16 @@ light_channel = 0;
 while True:
 
   # read the analog pin
-  light_level = readadc(light_channel)
+  light_level = ReadChannel(light_channel)
   light_volt = (light_level) * 3.3 / 1023
   
-  temp_level = readadc(temp_channel)
+  temp_level = ReadChannel(temp_channel)
   temp_volt = (temp_level) * 3.3 / 1023
   temp = (temp_volt - 0.5) * 100
   
   print "Light : " + str(light_level)
   print "Temp  : " + str(temp_level)
-  
 
-
-  
   print "Light V : " + str(light_volt)
   print "Temp  V : " + str(temp_volt)  
   
