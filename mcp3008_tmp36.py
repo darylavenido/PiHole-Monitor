2a473a41c0a9736ba1dbda8a70fd59e60d2479ac
Matt Hawkins <matthawkinsuk@gmail.com>
2013-10-13 23:34:37 +0100

    Added ConvertTemp function to handle TMP36 data.

--- mcp3008_tmp36.py
+++ mcp3008_tmp36.py
@@ -1,40 +1,70 @@
+#!/usr/bin/python
+#--------------------------------------   
+# This script reads data from a 
+# MCP3008 ADC device using the SPI bus.
+#
+# Author : Matt Hawkins
+# Date   : 13/10/2013
+#
+# http://www.raspberrypi-spy.co.uk/
+#
+#--------------------------------------
+
 import spidev
 import time
 import os
 
+# Open SPI bus
 spi = spidev.SpiDev()
 spi.open(0,0)
 
 # Function to read SPI data from MCP3008 chip
+# Channel must be an integer 0-7
 def ReadChannel(channel):
-  if ((channel > 7) or (channel < 0)):
-    return -1
-  r = spi.xfer2([1,(8+channel)<<4,0])
-  adcout = ((r[1]&3) << 8) + r[2]
-  return adcout
+  adc = spi.xfer2([1,(8+channel)<<4,0])
+  data = ((adc[1]&3) << 8) + adc[2]
+  return data
 
-temp_channel = 1;
-light_channel = 0;
+# Function to convert data to voltage level,
+# rounded to specified number of decimal places. 
+def ConvertVolts(data,places):
+  volts = (data * 3.3) / 1023
+  volts = round(volts,places)  
+  return volts
+  
+# Function to calculate temperature from
+# TMP36 data, rounded to specified
+# number of decimal places.
+def ConvertTemp(data,places):
+  temp = ((data * 330)/1023)-50
+  temp = round(temp,places)
+  return temp
+  
+# Define sensor channels
+light_channel = 0
+temp_channel  = 1
+
+# Define delay between readings
+delay = 5
 
 while True:
 
-  # read the analog pin
+  # Read the light sensor data
   light_level = ReadChannel(light_channel)
-  light_volt = (light_level) * 3.3 / 1023
-  
-  temp_level = ReadChannel(temp_channel)
-  temp_volt = (temp_level) * 3.3 / 1023
-  temp = (temp_volt - 0.5) * 100
-  
-  print "Light : " + str(light_level)
-  print "Temp  : " + str(temp_level)
+  light_volts = ConvertVolts(light_level,2)
 
-  print "Light V : " + str(light_volt)
-  print "Temp  V : " + str(temp_volt)  
-  
-  print "Temp = " + str(temp) + " degrees"
-  
-  print "-------------------------"
-  
-  time.sleep(5)
+  # Read the temperature sensor data
+  temp_level = ReadChannel(temp_channel)
+  temp_volts = ConvertVolts(temp_level,2)
+  temp       = ConvertTemp(temp_level,2)
+
+  # Print out results
+  print "--------------------------------------------"  
+  print("Light : {} ({}V)".format(light_level, light_volts))  
+  print("Temp  : {} ({}V) {} deg C".format(temp_level, temp_volts, temp))    
+  #print "Light : " + str(light_level) + " (" + str(light_volts) + "V)"
+  #print "Temp  : " + str(temp_level) + " (" + str(temp_volts) + "V) " + str(temp) + " degrees C" 
+
+  # Wait before repeating loop
+  time.sleep(delay)
  
