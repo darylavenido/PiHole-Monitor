a36cac0e50b39457880f91bf6b3129bf38c9da72
Matt Hawkins <matthawkinsuk@gmail.com>
2015-03-20 18:00:00 +0000

    Tidied up code and added ability to do forward or reverse in either 1 or 2 step increments.

--- python/stepper.py
+++ python/stepper.py
@@ -11,15 +11,8 @@
 # A simple script to control
 # a stepper motor.
 #
-# Call either as :
-#   sudo python stepper.py
-# or :
-#   sudo python stepper.py 20
-# where 20 is the wait time between steps
-# in milliseconds. Default is 10.
-#
 # Author : Matt Hawkins
-# Date   : 08/03/2015
+# Date   : 20/03/2015
 #
 # http://www.raspberrypi-spy.co.uk/
 #
@@ -46,59 +39,48 @@ for pin in StepPins:
   GPIO.setup(pin,GPIO.OUT)
   GPIO.output(pin, False)
 
-# Define some settings
-StepCounter = 0
-
-# Look for wait time passed (in milliseconds)
-# as argument on the command line
-if len(sys.argv)>1:
-  WaitTime = int(sys.argv[1])/1000
-else:
-  WaitTime = 10/1000
-
-# Define simple sequence
-StepCount1 = 4
-Seq1 = []
-Seq1 = range(0, StepCount1)
-Seq1[0] = [1,0,0,0]
-Seq1[1] = [0,1,0,0]
-Seq1[2] = [0,0,1,0]
-Seq1[3] = [0,0,0,1]
-
 # Define advanced sequence
 # as shown in manufacturers datasheet
-StepCount2 = 8
-Seq2 = []
-Seq2 = range(0, StepCount2)
-Seq2[0] = [1,0,0,0]
-Seq2[1] = [1,1,0,0]
-Seq2[2] = [0,1,0,0]
-Seq2[3] = [0,1,1,0]
-Seq2[4] = [0,0,1,0]
-Seq2[5] = [0,0,1,1]
-Seq2[6] = [0,0,0,1]
-Seq2[7] = [1,0,0,1]
+Seq = [[1,0,0,0],
+       [1,1,0,0],
+       [0,1,0,0],
+       [0,1,1,0],
+       [0,0,1,0],
+       [0,0,1,1],
+       [0,0,0,1],
+       [1,0,0,1]]
+       
+StepCount = len(Seq)-1
+StepDir = 2 # Set to 1 or 2 for clockwise
+            # Set to -1 or -2 for anti-clockwise
 
-# Choose a sequence to use
-Seq = Seq2
-StepCount = StepCount2
+# Read wait time from command line
+if len(sys.argv)>1:
+  WaitTime = int(sys.argv[1])/float(1000)
+else:
+  WaitTime = 10/float(1000)
+
+# Initialise variables
+StepCounter = 0
 
 # Start main loop
-while 1==1:
+while True:
 
   for pin in range(0, 4):
     xpin = StepPins[pin]
+    print StepCounter
+    print pin
     if Seq[StepCounter][pin]!=0:
       print " Step %i Enable %i" %(StepCounter,xpin)
       GPIO.output(xpin, True)
     else:
       GPIO.output(xpin, False)
 
-  StepCounter += 1
+  StepCounter += StepDir
 
   # If we reach the end of the sequence
   # start again
-  if (StepCounter==StepCount):
+  if (StepCounter>=StepCount):
     StepCounter = 0
   if (StepCounter<0):
     StepCounter = StepCount
