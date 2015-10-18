4b9fe459cc0bb639664cbd9b45899274966c1917
Matt Hawkins <matthawkinsuk@gmail.com>
2015-10-18 21:10:07 +0100

    Added GPLv3 header to script.

--- python/lcd_i2c.py
+++ python/lcd_i2c.py
@@ -6,7 +6,7 @@
 # /_/|_/_/  /_/___/ .__/\_, /
 #                /_/   /___/
 #
-#           lcd_i2c.py
+#  lcd_i2c.py
 #  LCD test script using I2C backpack.
 #  Supports 16x2 and 20x4 screens.
 #
@@ -14,6 +14,21 @@
 # Date   : 20/09/2015
 #
 # http://www.raspberrypi-spy.co.uk/
+#
+# Copyright 2015 Matt Hawkins
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #--------------------------------------
 import smbus
