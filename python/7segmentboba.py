1d73392a30a0871a61706056a017cdd1e9ff6c5e
Matt Hawkins <matthawkinsuk@gmail.com>
2016-02-28 00:09:53 +0000

    Updated comments and added command line arguments to change mode.

--- python/7segmentboba.py
+++ python/7segmentboba.py
@@ -1,4 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+#--------------------------------------
+#    ___  ___  _ ____          
+#   / _ \/ _ \(_) __/__  __ __ 
+#  / , _/ ___/ /\ \/ _ \/ // / 
+# /_/|_/_/  /_/___/ .__/\_, /  
+#                /_/   /___/   
+#
+#  MAX7219 Seven Segment Display
+#  Boba Fett Chestplate Display
+#
+# By default it will display random characters
+# on all segments. Pass "random" or "seq" on
+# the command line to change mode.
+# e.g. python 7segmentboba.py seq
+#
+# The predefined sequence is based on information
+# posted by "RafalFett" on TheDentedHelmet.com
+# http://www.thedentedhelmet.com/f20/esb-chest-display-sequence-43686/
+#
+# Author : Matt Hawkins
+# Date   : 27/02/2016
+#
+# http://www.raspberrypi-spy.co.uk/
+#
+#--------------------------------------
 
 import max7219.led as led
 import sys
@@ -24,7 +49,7 @@ device.clear()
 
 interval = 1.2
 
-starttext = 'B0BA8ETT'
+starttext = 'B0BAFETT'
 
 sequence = [' dmeug  ',
             ' 88888  ',
@@ -46,15 +71,22 @@ for index, char in enumerate(starttext):
 
 time.sleep(1)
 
-# Display random symbols
-while mode=='random':
-  for index in range(1,9):
-    device.letter(0, index, getRandomChar())  
-  time.sleep(interval)
+try:
 
-# Display sequence
-while mode=='seq':
-  for item in sequence:
-    for index, char in enumerate(item):
-      device.letter(0, 8-index, char)      
+  # Display random symbols
+  print("Random sequence")
+  while mode=='random':
+    for index in range(1,9):
+      device.letter(0, index, getRandomChar())  
     time.sleep(interval)
+
+  # Display sequence
+  print("Predefined sequence")  
+  while mode=='seq':
+    for item in sequence:
+      for index, char in enumerate(item):
+        device.letter(0, 8-index, char)      
+      time.sleep(interval)
+    
+except KeyboardInterrupt:
+  print("Stop")
