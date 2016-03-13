2767e50f691e27963ce101efca6d65c1e301c561
Matt Hawkins <matthawkinsuk@gmail.com>
2016-03-13 21:12:35 +0000

    Added RAM, disk and interface information.

--- python/mypi.py
+++ python/mypi.py
@@ -9,14 +9,18 @@
 #  - Serial number
 #  - Mac address
 #  - IP address
+#  - CPU temperature
+#  - GPU temperature
 #
 # Author : Matt Hawkins
-# Date   : 09/03/2016
+# Date   : 12/03/2016
 #
 # http://www.raspberrypi-spy.co.uk/
 #
 #--------------------------------------
 
+import platform
+import subprocess
 import os
 
 # Define functions
@@ -79,16 +83,133 @@ def getIP(interface='eth0'):
   except:
     return 'Error'
 
+def getCPUtemp():
+  # Extract CPU temp
+  try:
+    temp = subprocess.check_output(['vcgencmd','measure_temp'])
+    temp = temp[5:-3]
+  except:
+    temp = '0.0'
+  temp = '{0:.2f}'.format(float(temp))
+  return str(temp)
+
+def getGPUtemp():
+  # Extract GPU temp
+  try:
+    temp = subprocess.check_output(['cat','/sys/class/thermal/thermal_zone0/temp'])
+    temp = float(temp)/1000
+  except:
+    temp = 0.0
+  temp = '{0:.2f}'.format(temp)
+  return temp
+
+def getRAM():
+  # free -m
+  output = subprocess.check_output(['free','-m'])
+  lines = output.splitlines()
+  line  = lines[1]
+  ram = line.split()
+  return (str(ram[1]),str(ram[2]),str(ram[3]))
+
+def getDisk():
+  # df -h
+  output = subprocess.check_output(['df','-h'])
+  lines = output.splitlines()
+  line  = lines[1]
+  disk  = line.split()
+  return disk
+
+def getCPUspeed():
+  # Get CPU frequency
+  try:
+    output = subprocess.check_output(['vcgencmd','get_config','arm_freq'])
+    lines = output.splitlines()
+    line  = lines[0]
+    freq = line.split('=')
+    freq = freq[1]
+  except:
+    freq = '0'
+  return freq
+
+def getUptime():
+  # uptime
+  # tupple uptime, 5 min load average
+  return 0
+
+def getPython():
+  # Get current Python version
+  # returns string
+  pythonv = platform.python_version()
+  return pythonv
+
+def getSPI():
+  # Check if SPI bus is enabled
+  # by checking for spi_bcm2### modules
+  # returns a string
+  spi = "False"
+  try:
+    c=subprocess.Popen("lsmod",stdout=subprocess.PIPE)
+    gr=subprocess.Popen(["grep" ,"spi_bcm2"],stdin=c.stdout,stdout=subprocess.PIPE)
+    output = gr.communicate()[0]
+    if output[:8]=='spi_bcm2':
+      spi = "True"
+  except:
+    pass
+  return spi
+
+def getI2C():
+  # Check if I2C bus is enabled
+  # by checking for i2c_bcm2### modules
+  # returns a string
+  i2c = "False"
+  try:
+    c=subprocess.Popen("lsmod",stdout=subprocess.PIPE)
+    gr=subprocess.Popen(["grep" ,"i2c_bcm2"],stdin=c.stdout,stdout=subprocess.PIPE)
+    output = gr.communicate()[0]
+    if output[:8]=='i2c_bcm2':
+      i2c = "True"
+  except:
+    pass
+  return i2c
+
+def getBT():
+  # Check if Bluetooth module is enabled
+  # returns a string
+  bt = "False"
+  try:
+    c=subprocess.Popen("lsmod",stdout=subprocess.PIPE)
+    gr=subprocess.Popen(["grep" ,"bluetooth"],stdin=c.stdout,stdout=subprocess.PIPE)
+    output = gr.communicate()[0]
+    if output[:9]=='bluetooth':
+      bt = "True"
+  except:
+    pass
+  return bt
 
 if __name__ == '__main__':
 
+  myRAM = getRAM()
+  myDisk = getDisk()
+
   # Script has been called directly
-  print "----------------------------------------"  
-  print "Revision Number      : " + getRevision()
-  print "Serial Number        : " + getSerial()
-  print "----------------------------------------"
-  print "Ethernet MAC Address : " + getMAC('eth0')
-  print "Ethernet IP Address  : " + getIP('eth0')
-  print "Wireless MAC Address : " + getMAC('wlan0')
-  print "Wireless IP Address  : " + getIP('wlan0')  
-  print "----------------------------------------"
+  print("----------------------------------------")
+  print("System               : " + platform.platform())
+  print("Revision Number      : " + getRevision())
+  print("Serial Number        : " + getSerial())
+  print("Python version       : " + platform.python_version())
+  print("----------------------------------------")
+  print("i2c enabled          : " + getI2C())
+  print("SPI enabled          : " + getSPI())
+  print("Bluetooth enabled    : " + getBT())
+  print("----------------------------------------")
+  print("Ethernet MAC Address : " + getMAC('eth0'))
+  print("Ethernet IP Address  : " + getIP('eth0'))
+  print("Wireless MAC Address : " + getMAC('wlan0'))
+  print("Wireless IP Address  : " + getIP('wlan0'))
+  print("----------------------------------------")
+  print("CPU Clock            : " + getCPUspeed() + "MHz")
+  print("CPU Temperature      : " + getCPUtemp() + "\xb0" +"C")
+  print("GPU Temperature      : " + getGPUtemp() + "\xb0" +"C")
+  print("RAM (Available)      : " + myRAM[0] + "MB (" + myRAM[2] + "MB)")
+  print("Disk (Available)     : " + myDisk[1] + " (" + myDisk[3] + ")")
+  print("----------------------------------------")
