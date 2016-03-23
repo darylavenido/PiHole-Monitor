7817a6c678a1571fb34ea4255540960754e789b1
MattHawkinsUK <matthawkinsuk@gmail.com>
2016-03-23 23:00:26 +0000

    Started to add code to detect treasure collection.

--- minecraft/tombrobber.py
+++ minecraft/tombrobber.py
@@ -18,44 +18,67 @@
 import mcpi.minecraft as minecraft
 import mcpi.block as block
 import random
+import time
 
 #--------------------------------------
 # Define Functions
 #--------------------------------------
 
-def createTomb(pos,size,blockID):
-  # A tomb at a set position with a given material
-  x1 = pos[0] + (size/2)
-  y1 = pos[1]
-  z1 = pos[2] + (size/2)
-  x2 = pos[0] - (size/2)
-  y2 = pos[1] - size
-  z2 = pos[2] - (size/2)
-  mc.setBlocks(x1,y1,z1,x2,y2,z2,blockID) 
-  mc.setBlocks(x1-1,y1-1,z1-1,x2+1,y2+1,z2+1,block.AIR.id) 
-  
-def addTreasure(pos,size,blockID):
-  # Add treasure to tomb at position "pos"
-  x = pos[0]
-  y = pos[1] - (size-1)
-  z = pos[2]
-  mc.setBlock(x,y,z,blockID)
-  
 def getLocation():
-  pritn
+  # Find suitable location for tomb
   scan=True
   while scan==True:
+	# Random x,z location
     x = random.randint(-120, 120)
     z = random.randint(-120, 120)  
-    y = mc.getHeight(x,z)
+    # Get height of first non-air block in column
+    y = mc.getHeight(x,z)-1
   
-    # check what block we have hit  
-    if mc.getBlock(x,y-1,z) in [block.SAND.id,block.DIRT.id,block.GRASS.id]:
+    # Check what block we have found
+    blocks = [block.SAND.id,block.DIRT.id,block.GRASS.id]
+    if mc.getBlock(x,y,z) in blocks:
+	  # Suitable block so end scan
       scan=False
-  y=y-3
+      
   pos=(x,y,z)
   
   return pos
+
+def createTomb(pos,size,depth,material,treasure):
+  # Create tomb at a set position with a given material
+  # Return position of treasure block
+  x1 = pos[0] + (size/2)
+  y1 = pos[1] - depth
+  z1 = pos[2] + (size/2)
+  x2 = pos[0] - (size/2)
+  y2 = pos[1] - depth - size
+  z2 = pos[2] - (size/2)
+  mc.setBlocks(x1,y1,z1,x2,y2,z2,material) 
+  mc.setBlocks(x1-1,y1-1,z1-1,x2+1,y2+1,z2+1,block.AIR.id) 
+  
+  # Add treasure to tomb at position "pos"
+  x = pos[0]
+  y = pos[1] - (size-1) - depth
+  z = pos[2]
+  mc.setBlock(pos[0],pos[1] - depth - size + 1,pos[2],treasure)
+  
+  return [x,y,z]
+  
+def placeMarker(pos,material):
+  # Place marker above tomb
+  mc.setBlock(pos[0],pos[1]+20,pos[2],material)    
+ 
+def checkTreasures():
+  for treasure in treasureList:
+	  
+	  pos = treasure[0]
+	  size = treasure[1]
+	  depth = treasure[2]
+	  x = pos[0]
+      y = pos[1] - (size-1) - depth
+      z = pos[2]
+	  if mc.getBlock(x,y,z)==block.AIR.id:
+        # Treasure gone
   
 #--------------------------------------
 #
@@ -67,19 +90,32 @@ mc = minecraft.Minecraft.create()
 
 random.seed
 
+treasureList = []
+
+tombCount = 8
+
 mc.postToChat("Let's rob some graves!")
 
-print("Place 5 random tombs and treasure")
+print("Place " str(tombCount) + " random tombs and treasure")
 
-for tomb in range[5]:
+for tomb in range[tombCount]:
   
-  pos = getLocation()
+  tombpos = getLocation()
   
   # Place marker in the sky
-  mc.setBlock(pos[0],pos[1]+20,pos[2],block.GLASS.id)  
-  createTomb(pos,4,block.STONE.id)  
-  addTreasure(pos,4,block.GOLD_BLOCK.id)
+  placeMarker(tombpos,block.GLASS.id)
+  treasurePos = createTomb(tombpos,4,3,block.STONE.id,block.GOLD_BLOCK.id)  
+  treasureList.append(treasurePos)
  
-  print("Tomb "+ tomb + " placed "+pos) 
+  print("Tomb " + str(tomb) + " placed "+pos) 
 
 # mc.player.setPos(0,30,4)
+
+remainingTreasure = tombCount
+
+mc.postToChat("There are " + str(remainingTreasure) + " treasures to find")
+
+while len(treasureList)>0:
+	# There are treasures to find!
+	checkTreasures():
+    time.sleep(5)
