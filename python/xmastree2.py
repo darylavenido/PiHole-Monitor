b45a31e2ba74d19eaf890684d060a4ad0330a1db
Matt Hawkins <matthawkinsuk@gmail.com>
2016-12-18 20:38:08 +0000

    Initial script for RGB Christmas tree add-on.

---
+++ python/xmastree2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#--------------------------------------
+#
+#            Raspberry Pi
+#        RGB Xmas Tree Add-on
+#
+# Randomly lights LEDs with a colour
+#
+# Author      : Andrew Gale
+# Modified By : Matt Hawkins
+# Date        : 18/12/2016
+# Based on example "rgb_tree_example.py" from
+# http://www.pocketmoneytronics.co.uk/
+#
+# Requires installation of Unicorn HAT librares.
+#
+# http://www.raspberrypi-spy.co.uk/
+#
+#--------------------------------------
+
+# Place holder file for RGB christmas tree script
