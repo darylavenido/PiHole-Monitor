076ac3afecd24ae9299bb4dc79dc655f1a8b6f90
Matt Hawkins <matthawkinsuk@gmail.com>
2017-01-15 11:50:44 +0000

    Updated to work with Python 2 and Python 3. Now use PiCamera module rather than raspistill.

--- camera/pi_camera_options.py
+++ camera/pi_camera_options.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/usr/bin/python
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 #|R|a|s|p|b|e|r|r|y|P|i|-|S|p|y|.|c|o|.|u|k|
@@ -74,3 +75,107 @@ try:
 except KeyboardInterrupt:
   # User quit
   print("\nGoodbye!")
+=======
+#!/usr/bin/python
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#|R|a|s|p|b|e|r|r|y|P|i|-|S|p|y|.|c|o|.|u|k|
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#
+# pi_camera_options.py
+# Takes a sequence of photos with the Pi camera
+# using a range of Exposure and White Balance
+# settings.
+#
+# Project URL :
+# http://www.raspberrypi-spy.co.uk/?p=1862
+#
+# Author : Matt Hawkins
+# Date   : 14/01/2017
+import os
+import time
+#import subprocess
+from picamera import PiCamera
+
+# Full list of Exposure and White Balance options
+#list_ex  = ['off','auto','night','nightpreview','backlight',
+#            'spotlight','sports','snow','beach','verylong',
+#            'fixedfps','antishake','fireworks']
+#list_awb = ['off','auto','sun','cloud','shade','tungsten',
+#            'fluorescent','incandescent','flash','horizon']
+
+# Refined list of Exposure and White Balance options. 60 photos.
+#list_ex  = ['off','auto','night','backlight','spotlight','fireworks']
+#list_awb = ['off','auto','sun','cloud','shade','tungsten','fluorescent','incandescent','flash','horizon']
+
+# Test list of Exposure and White Balance options. 6 photos.
+list_ex  = ['off','auto']
+list_awb = ['off','auto','sun']
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
+#camera.start_preview()
+# Camera warm-up time
+sleep(2)
+camera.capture('foo.jpg')
+camera.annotate_foreground = picamera.Color('black')
+camera.annotate_foreground = picamera.Color('white')
+camera.annotate_text = 'Test image'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# EV level
+photo_ev = 0
+
+# Photo dimensions and rotation
+photo_width  = 640
+photo_height = 480
+photo_rotate = 90
+
+photo_interval = 0.25 # Interval between photos (seconds)
+photo_counter  = 0    # Photo counter
+
+total_photos = len(list_ex) * len(list_awb)
+
+# Delete all previous image files
+try:
+  os.remove("photo_*.jpg")
+except OSError:
+  pass
+
+# Lets start taking photos!
+try:
+
+  print "Starting photo sequence"
+
+  for ex in list_ex:
+    for awb in list_awb:
+      photo_counter = photo_counter + 1
+      filename = 'photo_' + ex + '_' + awb + '.jpg'
+      camera.capture(filename)
+      camera.annotate_foreground = picamera.Color('black')
+      camera.annotate_foreground = picamera.Color('white')
+      camera.annotate_text = filename
+      #cmd = 'raspistill -o ' + filename + ' -t 1000 -ex ' + ex + ' -awb ' + awb + ' -ev ' + str(photo_ev) + ' -w ' + str(photo_width) + ' -h ' + str(photo_height) + ' -rot ' + str(photo_rotate)
+      #pid = subprocess.call(cmd, shell=True)
+      #print ' [' + str(photo_counter) + ' of ' + str(total_photos) + '] ' + filename    
+      time.sleep(photo_interval)
+  
+  print "Finished photo sequence"
+  
+except KeyboardInterrupt:
+  # User quit
+  print "\nGoodbye!"
+>>>>>>> 1543591c4e4079cd098367eedf8e7960628c91bf
