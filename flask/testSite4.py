e76bd98af7f859af9b1bf6411a571ecfb99c9c32
Matt Hawkins <matthawkinsuk@gmail.com>
2017-07-01 22:15:27 +0100

    Corrected typo in post_id

--- flask/testSite4.py
+++ flask/testSite4.py
@@ -19,7 +19,7 @@ def show_user(username):
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     # show the post with the given id, the id is an integer
-    return 'Post %d' % post_i
+    return 'Post %d' % post_id
  
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
