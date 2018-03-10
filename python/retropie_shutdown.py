ed52a9562b18e43f9c76b2f84ffde5862ec51a3c
Matt Hawkins <matthawkinsuk@gmail.com>
2018-03-10 22:02:41 +0000

    Initial upload of RetroPie shutdown button script.

---
+++ python/retropie_shutdown.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+#--------------------------------------
+#    ___  ___  _ ____          
+#   / _ \/ _ \(_) __/__  __ __ 
+#  / , _/ ___/ /\ \/ _ \/ // / 
+# /_/|_/_/  /_/___/ .__/\_, /  
+#                /_/   /___/   
+#
+#  RetroPie Shutdown Button Script
+#
+# This script enables a RetroPie based
+# system to shutdown by pressing a button
+# without needing to use the menu.
+# 
+# It uses the GpioZero library.
+#
+# Please see https://www.raspberrypi-spy.co.uk/
+# for more information.
+#
+# Author : Matt Hawkins
+# Date   : 10/03/2018
+#
+#--------------------------------------
+
+from gpiozero import Button
+from subprocess import check_call
+from signal import pause
+
+# Define GPIO number
+myGPIO=22
+
+# Define number of seconds button should be pressed
+myHoldTime=6
+
+def buttonHeld():
+  print("Button pressed for "+ str(myHoldTime) +" seconds")
+  check_call(['sudo', 'poweroff'])
+
+button=Button(myGPIO,pull_up=False,hold_time=myHoldTime)
+button.when_held=buttonHeld
+
+pause ()
