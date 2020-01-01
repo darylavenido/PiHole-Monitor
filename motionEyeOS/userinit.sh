3d4a6f993172fef3d53a6c535141536d1382db14
Matt Hawkins <matthawkinsuk@gmail.com>
2020-01-01 16:56:50 +0000

    Added double quotes around the location variable so it works when the location contains spaces.

--- motionEyeOS/userinit.sh
+++ motionEyeOS/userinit.sh
@@ -3,9 +3,9 @@ user="replace_me_with_your_user_key"
 token="replace_me_with_your_api_token"
 
 sleep 10
-
+ 
 port="40000"
 ip=$(curl -s https://api.ipify.org)
 ip="$ip:$port"
 
-python /data/pushover_boot.py $location $ip $user $token > /var/log/pushover &
+python /data/pushover_boot.py "$location" $ip $user $token > /var/log/pushover &
