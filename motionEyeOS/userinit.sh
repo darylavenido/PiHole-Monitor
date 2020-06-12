1da0d41cc2719f0cb36e1f1a583e3d80ad9039e8
MattHawkinsUK <matthawkinsuk@gmail.com>
2020-06-12 14:39:56 +0100

    Increase wait to allow network to stabilise. Removed concept of "location" because the Hostname is now used to refer to the system.

--- motionEyeOS/userinit.sh
+++ motionEyeOS/userinit.sh
@@ -1,11 +1,27 @@
-location="My House"
+#--------------------------------------
+#    ___  ___  _ ____
+#   / _ \/ _ \(_) __/__  __ __
+#  / , _/ ___/ /\ \/ _ \/ // /
+# /_/|_/_/  /_/___/ .__/\_, /
+#                /_/   /___/
+#
+#           userinit.sh
+#  Send pushover notification when motionEyeOS boots.
+#  Use with pushover_boot.py
+#
+# Author : Matt Hawkins
+# Date   : 12/06/2020
+#
+# https://www.raspberrypi-spy.co.uk/tag/motioneyeos/
+#
+#--------------------------------------
 user="replace_me_with_your_user_key"
 token="replace_me_with_your_api_token"
-
-sleep 10
- 
 port="40000"
+
+sleep 15
+
 ip=$(curl -s https://api.ipify.org)
 ip="$ip:$port"
 
-python /data/pushover_boot.py "$location" $ip $user $token > /var/log/pushover &
+python /data/pushover_boot.py $ip $user $token > /var/log/pushover &
